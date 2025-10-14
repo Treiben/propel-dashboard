a0e0cb24092e0104cf7436635ded31d76a2ccd5a
Tatyana Asriyan <tasriyan@hotmail.com>
2025-10-14 12:51:20 -0500

    deleted old files

--- traditional/Propel.FeatureFlags.Dashboard.Api/Endpoints/Shared/BusinessValidatationResult.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace Propel.FeatureFlags.Dashboard.Api.Endpoints.Shared;
-
-public record BusinessValidationResult
-{
-	public bool IsValid { get; init; }
-	public Dictionary<string, string[]> Errors { get; init; } = [];
-}
